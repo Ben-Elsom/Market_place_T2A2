a2289d305e74550500cfb81a118a0368526052e9
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-05-16 16:56:58 +1000

    built form, create , made show page, implemented devise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
     before_action :set_questions, only: [:index, :new]
+    before_action :set_question, only: [:show, :destroy]
     def index
         
     end
@@ -9,16 +10,28 @@ class QuestionsController < ApplicationController
     end
 
     def new
-
+        @question = Question.new
     end
     
     def create
-
+        @question = Question.create(question_params)
+        redirect_to @question
     end
 
     def set_questions
         @questions = Question.all
-
     end
 
+    def set_question 
+        @question = Question.find(params[:id])
+    end
+
+    def destroy
+        @question.destroy
+        redirect_to root_path
+    end
+
+    def question_params
+        params.require(:question).permit(:title, :description, :prize, :response_cost )
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "date"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,5 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Question.destroy_all
 Question.create(title:"This is a question", description: "This is the description", prize: 2, response_cost: 0.1)
