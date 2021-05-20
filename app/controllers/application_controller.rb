26e4beaf790ec3a709ab5cbd356e07d41ad077d6
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-05-20 21:26:28 +1000

    user auth working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :forbidden
+
+    private
+
+    def forbidden 
+        flash[:alert] = "You are not authorized to perfomr that action."
+        redirect_back(fallback_location: root_path)
+    end
+
 end
