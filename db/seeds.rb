94bd977a4b002040de5186af1738dd4ee07fc670
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-05-22 13:40:22 +1000

    changed seed usernames

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ date4 = DateTime.now + 4.day
 date5 = DateTime.now + 5.day
 date6 = DateTime.now + 6.day
 
-user = User.create(email: "a@a.com", username: "test1", password: "123456", balance: 200)
-user2 = User.create(email: "a@b.com", username: "test2", password: "123456", balance: 200)
+user = User.create(email: "a@a.com", username: "Ben Elsom", password: "123456", balance: 200)
+user2 = User.create(email: "a@b.com", username: "Tim Moss", password: "123456", balance: 200)
 
 question1 = Question.create(title:"What should I call my speedy flower delivery service?", description: "So the idea is that you will request flowers to be sent to an address and we will deliver the flowers within the hour with your custom message.", prize: 50, response_cost: 0.10, closing_date_and_time: date, user: user)
 
@@ -57,3 +57,7 @@ Comment.create(body: "Matt", question: question6, user: user2)
 Comment.create(body: "Persia", question: question6, user: user2)
 Comment.create(body: "Doug", question: question6, user: user2)
 Comment.create(body: "Ewan", question: question6, user: user2)
+
+question7 = Question.create(title:"Who is the smartest person ever?", prize: 50, response_cost: 5.0, closing_date_and_time: date2, user: user)
+question8 = Question.create(title:"What is a reason to live?", description: "Not depressed just curious what your favourite parts about being alive are", prize: 50, response_cost: 5.0, closing_date_and_time: date2, user: user)
+
