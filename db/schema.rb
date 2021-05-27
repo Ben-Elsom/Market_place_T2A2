804996d91cc1f52967d90884a6a9a3e6a1fe65dc
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-05-27 16:47:23 +1000

    adding prize given migration:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_024159) do
+ActiveRecord::Schema.define(version: 2021_05_27_064353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_024159) do
     t.datetime "closing_date_and_time"
     t.boolean "active", default: true
     t.bigint "user_id", null: false
+    t.boolean "prize_given", default: false
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
